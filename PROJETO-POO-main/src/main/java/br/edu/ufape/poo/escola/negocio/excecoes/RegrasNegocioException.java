807fa547349a00b47ae698d4ca4b4baa5deec2b7
dungeonnemesis <toniviniciuspaixao@gmail.com>
2026-09-04 19:18:37 -0300

    limpando vestigios antigos

--- PROJETO-POO-main/src/main/java/br/edu/ufape/poo/escola/negocio/excecoes/RegrasNegocioException.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ufape.poo.escola.negocio.excecoes;
-
-public class RegrasNegocioException extends RuntimeException {
-
-    public RegrasNegocioException(String mensagem) {
-        super(mensagem);
-    }
-}
